73074dfbd3936ef02e7ac1ea3dc4375820ec0e0b
Nurul Amin <namin2750@gmail.com>
2024-07-04 21:07:31 +0600

    balance

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,8 +1,11 @@
-const Balance = () => {
+import getUserBalance from "@/app/actions/getUserBalance";
+
+const Balance = async () => {
+  const { balance } = await getUserBalance();
   return (
     <div>
       <h4>Your Balance</h4>
-      <h1>$600</h1>
+      <h1>${balance}</h1>
     </div>
   );
 };
