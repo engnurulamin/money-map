2cb1e5f2ae4f8e5578dc02b7bef8a49beefa8698
Nurul Amin <namin2750@gmail.com>
2024-07-11 23:34:04 +0600

    again

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: "dist",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
