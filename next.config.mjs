150a51e80f60726f0e7eb80cc9b2b50ff8816b66
Nurul Amin <namin2750@gmail.com>
2024-07-12 20:17:35 +0600

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Explicitly set the target to 'export' for static export
+  output: "export",
+
+  // Add any other configuration options you need
+  // For example, enabling React Strict Mode:
+  reactStrictMode: true,
+
+  // Other configurations
+};
 
 export default nextConfig;
